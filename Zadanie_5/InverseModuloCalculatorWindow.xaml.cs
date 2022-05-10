68321641e50ffe5dbf8f54cf7fa22b8f6f273a5c
Salomon1337 <salomonpl2000@gmail.com>
2022-05-10 10:50:16 +0200

    Add InverseModuloCalculatorWindow

--- Zadanie_5/InverseModuloCalculatorWindow.xaml.cs
+++ Zadanie_5/InverseModuloCalculatorWindow.xaml.cs
@@ -1,27 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Numerics;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace Zadanie_5
 {
-    /// <summary>
-    /// Interaction logic for InverseModuloCalculatorWindow.xaml
-    /// </summary>
     public partial class InverseModuloCalculatorWindow : Window
     {
         public InverseModuloCalculatorWindow()
         {
             InitializeComponent();
+        }
+
+        private void HelpButtonClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(
+            "Program znajduje rozwiązanie x równania a⋅x = 1 (mod m), " +
+            "czyli liczbę x = a⁻¹ (mod m)\n" +
+            "Odwrotność a⁻¹ liczby a w mnożeniu modulo m istnieje tylko, " +
+            "gdy a i m są względnie pierwsze, czyli największy wspólny dzielnik a i m jest równy 1.");
+        }
+
+        private void CalculateButtonClick(object sender, RoutedEventArgs e)
+        {
+            BigInteger a, m;
+            if (!BigInteger.TryParse(NumberTextBox.Text, out a))
+            {
+                MessageBox.Show("Podaj prawidłową liczbę a.");
+                return;
+            }
+            if (!BigInteger.TryParse(ModulusTextBox.Text, out m))
+            {
+                MessageBox.Show("Podaj prawidłową liczbę m.");
+                return;
+            }
+            var rsa = new RSA();
+            if (rsa.GreatestCommonDivisor(a, m) != 1)
+            {
+                InverseTextBox.Text = "Odwrotność liczby a nie istnieje, ponieważ a i m nie są względnie pierwsze.";
+                return;
+            }
+            InverseTextBox.Text = rsa.ModularInverse(a, m).ToString();
         }
     }
 }
