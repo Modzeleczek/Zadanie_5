6cc2911456fa96829d23aa3cda23868586cb68f4
Modzeleczek <lukasz.modzelewski1@gmail.com>
2022-05-10 11:14:56 +0200

    Implement text encryption and decryption
    Change xaml indentation to 2 spaces

--- Zadanie_5/EnDecryptorWindow.xaml.cs
+++ Zadanie_5/EnDecryptorWindow.xaml.cs
@@ -1,25 +1,13 @@
 ﻿using Microsoft.Win32;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Numerics;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace Zadanie_5
 {
-    /// <summary>
-    /// Interaction logic for EnDecryptorWindow.xaml
-    /// </summary>
     public partial class EnDecryptorWindow : Window
     {
         public EnDecryptorWindow()
@@ -31,6 +19,34 @@ namespace Zadanie_5
         {
             MessageBox.Show("Tekst/Plik zaszyfrowany jednym kluczem z pary (prywatnym lub publicznym) może być odszyfrowany odpowiadającym mu drugim kluczem z pary (odpowiednio publicznym lub prywatnym).");
         }
+
+        private void TransformTextButtonClick(object sender, RoutedEventArgs e)
+        {
+            BigInteger modulus, exponent;
+            try { ParseKey(out modulus, out exponent); }
+            catch (ArgumentException ex) { MessageBox.Show(ex.Message); return; }
+            var inputTextTypeIndex = CheckedIndex(InputTextTypeStackPanel.Children);
+            byte[] inputBytes;
+            if (inputTextTypeIndex == 0)
+                inputBytes = Encoding.UTF8.GetBytes(InputTextTextBox.Text);
+            else
+                try { inputBytes = HexStringToByteArray(InputTextTextBox.Text); }
+                catch (ArgumentException ex) { MessageBox.Show(ex.Message); return; }
+            var bigInt = new BigInteger(inputBytes); // tworzymy liczbę całkowitą z bajtów tekstu
+            var pow = new RSA().PowerModulo(bigInt, exponent, modulus); // potęgujemy liczbę modulo według podanego przez użytkownika modułu
+            var outputTextTypeIndex = CheckedIndex(OutputTextTypeStackPanel.Children);
+            var powBytes = pow.ToByteArray();
+            if (outputTextTypeIndex == 0)
+                OutputTextTextBox.Text = Encoding.UTF8.GetString(powBytes);
+            else
+                OutputTextTextBox.Text = ByteArrayToHexString(powBytes);
+        }
+
+        private string BytesToString(byte[] bytes)
+        {
+            return string.Join(", ", bytes);
+        }
+
         private void ParseKey(out BigInteger modulus, out BigInteger exponent)
         {
             if (!BigInteger.TryParse(ModulusTextBox.Text, out modulus))
@@ -38,6 +54,55 @@ namespace Zadanie_5
             if (!BigInteger.TryParse(ExponentTextBox.Text, out exponent))
                 throw new ArgumentException("Podaj prawidłowy wykładnik.");
         }
+
+        private int CheckedIndex(UIElementCollection radioButtons)
+        {
+            int index = 0;
+            foreach (var rb in radioButtons)
+            {
+                if ((bool)(rb as RadioButton).IsChecked)
+                    break;
+                ++index;
+            }
+            return index;
+        }
+
+        private string ByteArrayToHexString(byte[] ba)
+        {
+            var chars = new char[ba.Length * 2];
+            int ci = 0;
+            for (int bi = ba.Length - 1; bi >= 0; --bi)
+            {
+                /* var bStr = ba[bi].ToString("x2");
+                chars[ci++] = bStr[0];
+                chars[ci++] = bStr[1]; */
+                var b = ba[bi];
+                var bH = b >> 4;
+                chars[ci++] = (char)(bH <= 9 ? bH + '0' : bH - 10 + 'a');
+                var bL = b & 0b1111;
+                chars[ci++] = (char)(bL <= 9 ? bL + '0' : bL - 10 + 'a');
+            }
+            return new string(chars);
+        }
+
+        private byte[] HexStringToByteArray(string str)
+        {
+            if (str.Length % 2 != 0) // jeżeli str nie ma parzystej długości
+                throw new ArgumentException("str nie ma parzystej długości.");
+            var bytes = new byte[str.Length / 2];
+            int ci = str.Length - 1;
+            for (int bi = 0; bi < bytes.Length; ++bi)
+            {
+                var cL = str[ci--];
+                var cH = str[ci--];
+                int bInt = 0;
+                bInt |= (cH <= '9' ? cH - '0' : cH + 10 - 'a') << 4;
+                bInt |= cL <= '9' ? cL - '0' : cL + 10 - 'a';
+                bytes[bi] = (byte)bInt;
+            }
+            return bytes;
+        }
+
         private void SelectInputFileButtonClick(object sender, RoutedEventArgs e)
         {
             var dialog = new OpenFileDialog();
