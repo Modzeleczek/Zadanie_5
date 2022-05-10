6406290daeec31370a560fae23e0cf2e21be153a
Salomon1337 <salomonpl2000@gmail.com>
2022-05-10 11:11:12 +0200

    Add EnDecryptorWindow

--- Zadanie_5/EnDecryptorWindow.xaml.cs
+++ Zadanie_5/EnDecryptorWindow.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -22,6 +25,46 @@ namespace Zadanie_5
         public EnDecryptorWindow()
         {
             InitializeComponent();
+        }
+
+        private void HelpButtonClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Tekst/Plik zaszyfrowany jednym kluczem z pary (prywatnym lub publicznym) może być odszyfrowany odpowiadającym mu drugim kluczem z pary (odpowiednio publicznym lub prywatnym).");
+        }
+        private void ParseKey(out BigInteger modulus, out BigInteger exponent)
+        {
+            if (!BigInteger.TryParse(ModulusTextBox.Text, out modulus))
+                throw new ArgumentException("Podaj prawidłowy moduł.");
+            if (!BigInteger.TryParse(ExponentTextBox.Text, out exponent))
+                throw new ArgumentException("Podaj prawidłowy wykładnik.");
+        }
+        private void SelectInputFileButtonClick(object sender, RoutedEventArgs e)
+        {
+            var dialog = new OpenFileDialog();
+            dialog.Title = "Plik wejściowy";
+            dialog.InitialDirectory = Directory.GetCurrentDirectory();
+            // dialog.Filter = "Wszystkie pliki|*"; // aby wyświetlać listę z wyborem rodzaju pliku
+            if (dialog.ShowDialog() != true) // jeżeli użytkownik anulował
+                return;
+            InputFilePathTextBox.Text = dialog.FileName;
+        }
+
+        private void SaveAsFileButtonClick(object sender, RoutedEventArgs e)
+        {
+            var dialog = new SaveFileDialog();
+            dialog.Title = "Plik wyjściowy";
+            dialog.InitialDirectory = Directory.GetCurrentDirectory();
+            if (dialog.ShowDialog() != true) // jeżeli użytkownik anulował
+                return;
+            BigInteger modulus, exponent;
+            try { ParseKey(out modulus, out exponent); }
+            catch (ArgumentException ex) { MessageBox.Show(ex.Message); return; }
+            if (InputFilePathTextBox.Text.Length == 0)
+            { MessageBox.Show("Wybierz plik wejściowy."); return; }
+            var inputBytes = File.ReadAllBytes(InputFilePathTextBox.Text);
+            var bigInt = new BigInteger(inputBytes); // tworzymy liczbę całkowitą z bajtów pliku
+            var pow = new RSA().PowerModulo(bigInt, exponent, modulus); // potęgujemy liczbę modulo według podanego przez użytkownika modułu
+            File.WriteAllBytes(dialog.FileName, pow.ToByteArray());
         }
     }
 }
